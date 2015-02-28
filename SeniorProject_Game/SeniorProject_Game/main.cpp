e46a4f1227812ca13d876344a62b31dbde29d5cd
Loooplop <williamcpaul@hotmail.com>
2015-02-27 23:10:32 -0800

    Added Skeleton SFML

--- SeniorProject_Game/SeniorProject_Game/main.cpp
+++ SeniorProject_Game/SeniorProject_Game/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
 int main(void)
 {
+	sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(800, 600), "Mario Replica", sf::Style::Default);
 
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
+			{
+				window.close();
+			}
+
+
+		}
+		window.clear();
+
+
+		window.display();
+	}
 	return 0;
 }
